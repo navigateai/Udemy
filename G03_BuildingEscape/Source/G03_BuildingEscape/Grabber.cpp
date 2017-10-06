93f9444ace0e628254488dc651a0814d0950ff73
Tim Drake <timduk@timduk.com>
2017-10-06 17:16:51 +0100

    Collision detection algorithm for player & furniture

--- G03_BuildingEscape/Source/G03_BuildingEscape/Grabber.cpp
+++ G03_BuildingEscape/Source/G03_BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
+#include "Engine/World.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -30,6 +33,51 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get Player View point this tick
+
+	FVector PlayerViewpointLocation;
+	FRotator PlayerViewpointRotation;
+
+	// This is a "void" Get function
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		// Macro OUT does nothing, but signals that these are parameters modified by a Get function
+		OUT PlayerViewpointLocation,
+		OUT PlayerViewpointRotation);
+
+	// Log our viewpoint every tick
+	//UE_LOG(LogTemp, Warning, TEXT("Location is %s, Rotation is %s\n"),
+	//	*PlayerVector.ToString(), *PlayerRotator.ToString());
+
+	FVector LineTraceEnd = PlayerViewpointLocation + PlayerViewpointRotation.Vector() * Reach;
+
+	// Ray-cast out to reach distance
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewpointLocation,
+		LineTraceEnd,
+		FColor(0,255,0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
+
+	/// Set up Query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	/// Line-trace (AKA raay-cast) out to reach distance
+	FHitResult Hit;
+	bool GotHit =
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewpointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	// See what we hit
+	if(GotHit)
+		UE_LOG(LogTemp, Warning, TEXT("We hit a %s"), *Hit.Actor->GetName());
 }
 
